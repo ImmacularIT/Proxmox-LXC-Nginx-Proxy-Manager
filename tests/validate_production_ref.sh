b3003796193c82e6f50264b18f40e9bd52e7054c
ImmacularIT <immaculartist@gmail.com>
2026-08-08 23:39:06 +0200

    Guard production refs in both installers

--- tests/validate_production_ref.sh
+++ tests/validate_production_ref.sh
@@ -2,10 +2,14 @@
 set -Eeuo pipefail
 
 launcher="ct/nginx-proxy-manager.sh"
+installer="install/nginx-proxy-manager-install.sh"
 readme="README.md"
 
-grep -Fq 'PROJECT_REF="${NPM_PROJECT_REF:-main}"' "$launcher"
-! grep -Fq 'PROJECT_REF="${NPM_PROJECT_REF:-develop/' "$launcher"
+for file in "$launcher" "$installer"; do
+  grep -Fq 'PROJECT_REF="${NPM_PROJECT_REF:-main}"' "$file"
+  ! grep -Fq 'PROJECT_REF="${NPM_PROJECT_REF:-develop/' "$file"
+done
+
 grep -Fq 'https://raw.githubusercontent.com/ImmacularIT/Proxmox-LXC-Nginx-Proxy-Manager/main/ct/nginx-proxy-manager.sh' "$readme"
 ! grep -Fq '/develop/native-lxc-v2.15.1/ct/nginx-proxy-manager.sh' "$readme"
 
